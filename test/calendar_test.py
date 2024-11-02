9472874f87f53c12c2ead004f768282c4215b662
WispySparks <wispysparks@gmail.com>
2024-11-01 21:22:39 -0500

    formatting

--- test/calendar_test.py
+++ test/calendar_test.py
@@ -5,23 +5,28 @@ from google_calendar import GoogleCalendar
 
 calendar = GoogleCalendar()
 
+
 def invalidateToken():
     calendar.headers = {"Authorization": "Bearer "}
 
+
 def printToken():
     print("Access Token: " + calendar.headers.get("Authorization", "").removeprefix("Bearer "))
-    
+
+
 def printCalendars():
     print("Calendars: " + ", ".join(list(calendar.calendars.keys())))
+
 
 def printEvents():
     for c in calendar.calendars.keys():
         event = calendar.getNextEvent(c)
-        if (event is None):
+        if event is None:
             print(c + ": None")
             continue
         name, date, _ = event
         print(c + ": " + name + " - " + date.strftime(GoogleCalendar.dateFormat))
+
 
 printToken()
 printCalendars()
