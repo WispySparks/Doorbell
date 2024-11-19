7a9951d32673f50dbd68237693ec833fa7376c1b
WispySparks <wispysparks@gmail.com>
2024-11-18 18:32:33 -0600

    should be like this

--- test/app_test.py
+++ test/app_test.py
@@ -14,10 +14,8 @@ def fake_response(cmd: str) -> dict:
 def test_doorbell() -> None:
     """Takes input from the command line and feeds it into doorbell.
     Run from a thread because app.main() is blocking."""
-    while not app.slack_socket_handler.client.is_connected():
-        pass
     print(json.dumps(app.app.client.auth_test().data, indent=4))
-    while not app.slack_socket_handler.client.closed:
+    while True:
         cmd = input("Command: ")
         app.mention_event(fake_response(cmd), print)  # type: ignore
 
