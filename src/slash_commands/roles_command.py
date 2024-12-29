e6e2b937cc79ead2ecfcaca36d0dca67cef3721b
WispySparks <wispysparks@gmail.com>
2024-12-29 14:56:26 -0600

    make constants upper snake case

--- src/slash_commands/roles_command.py
+++ src/slash_commands/roles_command.py
@@ -21,69 +21,69 @@ from slack_sdk.models.views import View
 import database
 
 
-class RolesCommand:  # It'd be nice if it saved when you switched users instead of always having to hit Save.
+class RolesCommand:  # It'd be nice if it saved when you selected roles.
     """Contains all the functionality pertaining to the /roles command.
     To give your Slack app this command call register(app)."""
 
-    view_type: Final[str] = "modal"
-    root_callback_id: Final[str] = "roles_view"
-    manage_callback_id: Final[str] = "roles_view_manage"
-    view_title: Final[str] = "Roles"
-    view_submit: Final[str] = "Save"
+    VIEW_TYPE: Final[str] = "modal"
+    ROOT_CALLBACK_ID: Final[str] = "roles_view"
+    MANAGE_CALLBACK_ID: Final[str] = "roles_view_manage"
+    VIEW_TITLE: Final[str] = "Roles"
+    VIEW_SUBMIT: Final[str] = "Save"
 
-    manage_add_block_id: Final[str] = "roles_manage_add"
-    manage_add_action_id: Final[str] = manage_add_block_id
-    manage_remove_block_id: Final[str] = "roles_manage_remove"
-    manage_remove_action_id: Final[str] = manage_remove_block_id
+    MANAGE_ADD_BLOCK_ID: Final[str] = "roles_manage_add"
+    MANAGE_ADD_ACTION_ID: Final[str] = MANAGE_ADD_BLOCK_ID
+    MANAGE_REMOVE_BLOCK_ID: Final[str] = "roles_manage_remove"
+    MANAGE_REMOVE_ACTION_ID: Final[str] = MANAGE_REMOVE_BLOCK_ID
 
-    manage_button_action_id: Final[str] = "roles_manage"
-    manage_roles_button: Final[ActionsBlock] = ActionsBlock(
-        elements=[ButtonElement(text="Manage Roles", action_id=manage_button_action_id)]
+    MANAGE_BUTTON_ACTION_ID: Final[str] = "roles_manage"
+    MANAGE_ROLES_BUTTON: Final[ActionsBlock] = ActionsBlock(
+        elements=[ButtonElement(text="Manage Roles", action_id=MANAGE_BUTTON_ACTION_ID)]
     )
 
-    user_select_block_id: Final[str] = "roles_user_select"
-    user_select_action_id: Final[str] = user_select_block_id
-    user_select: Final[SectionBlock] = SectionBlock(
-        block_id=user_select_block_id,
+    USER_SELECT_BLOCK_ID: Final[str] = "roles_user_select"
+    USER_SELECT_ACTION_ID: Final[str] = USER_SELECT_BLOCK_ID
+    USER_SELECT: Final[SectionBlock] = SectionBlock(
+        block_id=USER_SELECT_BLOCK_ID,
         text="User:",
-        accessory=UserSelectElement(placeholder="Select a user", action_id=user_select_action_id),
+        accessory=UserSelectElement(placeholder="Select a user", action_id=USER_SELECT_ACTION_ID),
     )
 
-    role_select_action_id: Final[str] = "roles_role_select"
+    ROLE_SELECT_ACTION_ID: Final[str] = "roles_role_select"
 
     def register(self, app: App) -> None:
         """Registers your Slack app to have a /roles command."""
         app.command("/roles")(self._roles_command)
-        app.action(self.manage_button_action_id)(self._roles_manage)
-        app.action(self.role_select_action_id)(lambda ack: ack())  # Dummy because it's not an input
-        app.action(self.manage_remove_action_id)(lambda ack: ack())  # Dummy because it's not an input
-        app.action(self.user_select_action_id)(self._roles_user_select)
-        app.view_submission(self.root_callback_id)(self._roles_submit)
-        app.view_submission(self.manage_callback_id)(self._roles_manage_submit)
+        app.action(self.MANAGE_BUTTON_ACTION_ID)(self._roles_manage)
+        app.action(self.ROLE_SELECT_ACTION_ID)(lambda ack: ack())  # Dummy because it's not an input
+        app.action(self.MANAGE_REMOVE_ACTION_ID)(lambda ack: ack())  # Dummy because it's not an input
+        app.action(self.USER_SELECT_ACTION_ID)(self._roles_user_select)
+        app.view_submission(self.ROOT_CALLBACK_ID)(self._roles_submit)
+        app.view_submission(self.MANAGE_CALLBACK_ID)(self._roles_manage_submit)
 
     def _roles_command(self, ack: Ack, command: dict, client: WebClient) -> None:
         ack()
         client.views_open(
             trigger_id=command["trigger_id"],
             view=View(
-                type=self.view_type,
-                callback_id=self.root_callback_id,
-                title=self.view_title,
-                blocks=[self.manage_roles_button, self.user_select],
+                type=self.VIEW_TYPE,
+                callback_id=self.ROOT_CALLBACK_ID,
+                title=self.VIEW_TITLE,
+                blocks=[self.MANAGE_ROLES_BUTTON, self.USER_SELECT],
             ),
         )
 
     def _roles_manage(self, ack: Ack, body: dict, client: WebClient) -> None:
         ack()
-        user = body["view"]["state"]["values"][self.user_select_block_id][self.user_select_action_id]["selected_user"]
+        user = body["view"]["state"]["values"][self.USER_SELECT_BLOCK_ID][self.USER_SELECT_ACTION_ID]["selected_user"]
         options = self._generate_options(database.read().get_roles())
         blocks = [
             InputBlock(
                 label="Roles to add (space separated)",
                 optional=True,
-                block_id=self.manage_add_block_id,
+                block_id=self.MANAGE_ADD_BLOCK_ID,
                 element=PlainTextInputElement(
-                    placeholder="Business CAD Leads ...", action_id=self.manage_add_action_id
+                    placeholder="Business CAD Leads ...", action_id=self.MANAGE_ADD_ACTION_ID
                 ),
             )
         ]
@@ -91,17 +91,17 @@ class RolesCommand:  # It'd be nice if it saved when you switched users instead 
             blocks += [
                 SectionBlock(
                     text="Roles to remove",
-                    block_id=self.manage_remove_block_id,
-                    accessory=StaticMultiSelectElement(options=options, action_id=self.manage_remove_action_id),
+                    block_id=self.MANAGE_REMOVE_BLOCK_ID,
+                    accessory=StaticMultiSelectElement(options=options, action_id=self.MANAGE_REMOVE_ACTION_ID),
                 )
             ]
         client.views_push(
             trigger_id=body["trigger_id"],
             view=View(
-                type=self.view_type,
-                callback_id=self.manage_callback_id,
-                title=self.view_title,
-                submit=self.view_submit,
+                type=self.VIEW_TYPE,
+                callback_id=self.MANAGE_CALLBACK_ID,
+                title=self.VIEW_TITLE,
+                submit=self.VIEW_SUBMIT,
                 blocks=blocks,
                 private_metadata=user,
             ),
@@ -115,7 +115,7 @@ class RolesCommand:  # It'd be nice if it saved when you switched users instead 
         data = database.read()
         user_roles = self._generate_options(data.get_roles_for_user(user))
         options = self._generate_options(data.get_roles())
-        blocks = [self.manage_roles_button, self.user_select]
+        blocks = [self.MANAGE_ROLES_BUTTON, self.USER_SELECT]
         select_block_id = "".join(random.choices(string.ascii_letters + string.digits, k=10))
         submit = None
         if options and user != "":
@@ -124,17 +124,17 @@ class RolesCommand:  # It'd be nice if it saved when you switched users instead 
                     text="Roles",
                     block_id=select_block_id,
                     accessory=StaticMultiSelectElement(
-                        options=options, initial_options=user_roles, action_id=self.role_select_action_id
+                        options=options, initial_options=user_roles, action_id=self.ROLE_SELECT_ACTION_ID
                     ),
                 )
             ]
-            submit = self.view_submit
+            submit = self.VIEW_SUBMIT
         client.views_update(
             view_id=view_id,
             view=View(
-                type=self.view_type,
-                callback_id=self.root_callback_id,
-                title=self.view_title,
+                type=self.VIEW_TYPE,
+                callback_id=self.ROOT_CALLBACK_ID,
+                title=self.VIEW_TITLE,
                 submit=submit,
                 blocks=blocks,
                 private_metadata=select_block_id,
@@ -146,17 +146,17 @@ class RolesCommand:  # It'd be nice if it saved when you switched users instead 
         ack(
             response_action="update",
             view=View(
-                type=self.view_type,
-                callback_id=self.root_callback_id,
-                title=self.view_title,
-                submit=self.view_submit,
+                type=self.VIEW_TYPE,
+                callback_id=self.ROOT_CALLBACK_ID,
+                title=self.VIEW_TITLE,
+                submit=self.VIEW_SUBMIT,
                 blocks=view["blocks"],
                 private_metadata=private_metadata,
             ),
         )
         values = view["state"]["values"]
-        user = values[self.user_select_block_id][self.user_select_action_id]["selected_user"]
-        selected = values.get(private_metadata, {}).get(self.role_select_action_id, {}).get("selected_options", [])
+        user = values[self.USER_SELECT_BLOCK_ID][self.USER_SELECT_ACTION_ID]["selected_user"]
+        selected = values.get(private_metadata, {}).get(self.ROLE_SELECT_ACTION_ID, {}).get("selected_options", [])
         roles = {role["value"] for role in selected}
         data = database.read()
         data.set_roles(user, roles)
@@ -166,15 +166,15 @@ class RolesCommand:  # It'd be nice if it saved when you switched users instead 
 
     def _roles_manage_submit(self, ack: Ack, view: dict, body: dict, client: WebClient) -> None:
         ack()
-        roles_to_add = view["state"]["values"][self.manage_add_block_id][self.manage_add_action_id]["value"]
+        roles_to_add = view["state"]["values"][self.MANAGE_ADD_BLOCK_ID][self.MANAGE_ADD_ACTION_ID]["value"]
         if roles_to_add is None:
             roles_to_add = ""
         roles_to_add = roles_to_add.split()
         roles_to_remove = (
             view.get("state", {})
             .get("values", {})
-            .get(self.manage_remove_block_id, {})
-            .get(self.manage_remove_action_id, {})
+            .get(self.MANAGE_REMOVE_BLOCK_ID, {})
+            .get(self.MANAGE_REMOVE_ACTION_ID, {})
             .get("selected_options", [])
         )
         roles_to_remove = [option["value"] for option in roles_to_remove]
