ead6b850dbc043d55d3ac4d6a85e7887e7f01b6b
WispySparks <wispysparks@gmail.com>
2025-01-05 01:48:57 -0600

    move into a function

--- test/user_id_to_name.py
+++ test/user_id_to_name.py
@@ -9,11 +9,15 @@ app = App(token=BOT_TOKEN)
 slack_socket_handler = SocketModeHandler(app, APP_TOKEN)
 slack_socket_handler.connect()
 
+
+def user_id_to_name(user_id: str) -> str:
+    """Converts a Slack User ID into their name."""
+    return app.client.users_info(user=user_id).get("user", {}).get("real_name", "")
+
+
 try:
     while True:
-        user_id = input("ID: ")
-        name = app.client.users_info(user=user_id).get("user", {}).get("real_name", "")
-        print(name)
+        print(user_id_to_name(input("ID: ")))
 except KeyboardInterrupt:
     print("Stopping.")
     slack_socket_handler.close()
